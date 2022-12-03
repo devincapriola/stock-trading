aea306709326ce3a725972ad94964eef9e15aba2
Devin Capriola <devincapriola@gmail.com>
2022-12-03 15:30:52 -0500

    part 3

--- rl_trader.py
+++ rl_trader.py
@@ -255,3 +255,70 @@ class MultiStockEnv:
                         self.cash_in_hand -= self.stock_price[i]
                     else:
                         can_buy = False
+
+
+class DQNAgent(object):
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = ReplayBuffer(state_size, action_size, size=500)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.model = MLP(state_size, action_size)
+
+        # loss and optimizer
+        self.criterion = nn.MSELoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters())
+
+    def update_replay_memory(self, state, action, reward, next_state, done):
+        self.memory.add(state, action, reward, next_state, done)
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return np.random.choice(self.action_size)
+        act_values = predict(self.model, state)
+        return torch.argmax(act_values[0])  # returns action
+
+    def replay(self, batch_size):
+        # first check if replay buffer has enough samples
+        if self.memory.size < batch_size:
+            return
+
+        # sample a batch from the replay memory
+        minibatch = self.memory.sample_batch(batch_size)
+        states = minibatch['s']
+        actions = minibatch['a']
+        rewards = minibatch['r']
+        next_states = minibatch['s2']
+        done = minibatch['d']
+
+        # calculate targets
+        target = rewards + (1 - done) * self.gamma * \
+            np.amax(self.model.predict(next_states), axis=1)
+
+        # With the PyTorch API, it is simplest to have the target be the
+        # same shape as the predictions.
+        # However, we only need to update the network for the actions
+        # which were actually taken.
+        # We can accomplish this by setting the target to be equal to
+        # the prediction for all values.
+        # Then, only change the targets for the actions taken.
+        # Q(s,a)
+        target_full = predict(self.model, states)
+        target_full[np.arange(batch_size), actions] = target
+
+        # Run one training step
+        train_one_step(self.model, self.criterion,
+                       self.optimizer, states, target_full)
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
+
